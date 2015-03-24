0fda78aaba5cfba1a207691bf3e7c1870aa37737
Chris Warburton <chriswarbo@gmail.com>
2015-03-24 13:10:55 +0000

    Dump ASTs

--- src/Main.hs
+++ src/Main.hs
@@ -1,36 +1,17 @@
 module Main where
 
-import Data.Set as Set
-import Language.Haskell.Exts.Annotated hiding (ann)
-import Distribution.HaskellSuite.Modules
-import Distribution.HaskellSuite.Packages
-import Language.Haskell.Names
-import Language.Haskell.Names.Interfaces
+import Language.Haskell.Exts.Extension
+import Language.Haskell.Exts.Parser
+import Language.Haskell.Exts.SrcLoc
+import Language.Haskell.Exts.Syntax
 
---parseFile :: String -> IO (Module SrcLoc)
-parseF f = do let mode = defaultParseMode {parseFilename=f}
+parseFile :: String -> IO Module
+parseFile f = let mode  = defaultParseMode {parseFilename=f}
                   parse = parseModuleWithMode mode
-              src <- readFile f
-              return (fromParseResult (parse src))
+              in  fmap (fromParseResult . parse) (readFile f)
 
---ann :: Module SrcSpan -> ModuleT [Symbol] IO (Module (Scoped SrcSpan))
-ann = annotateModule Haskell2010 []
+parseFiles = mapM parseFile
 
-compInt = computeInterfaces Haskell2010 []
+dump (Module _ _ _ _ _ _ ds) = show ds
 
-compIntErr ms = do errs <- compInt ms
-                   if Set.null errs
-                      then return ms
-                      else error (show (elems errs))
-
-parseFiles = mapM parseF
-
-uniquify ms = compIntErr ms >>= mapM ann
-
---parseUniqueStrs ss = evalNamesModuleT undefined (uniquify ss)
-
-{-
-parseUnique fs = do parsed <- parseFiles fs
-                    evalNamesModuleT undefined (uniquify parsed)
--}
---main = evalNamesModuleT (parseUnique ["tests/data/arith.hs"]) (getInstalledPackages)
+main = parseFiles ["tests/data/arith.hs"]
