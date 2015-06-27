2782269fc4beecfbbbc8aea94102b155b979ee3f
Chris Warburton <chriswarbo@gmail.com>
2015-06-27 13:15:53 +0100

    Remove GHC stuff

--- src/HS2AST/Types.hs
+++ src/HS2AST/Types.hs
@@ -1,19 +1,10 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 
 module HS2AST.Types (
-   HsFile()
- , Sexpr(Leaf, Node)
+   Sexpr(Leaf, Node)
  , mkLeaf
  , mkNode
- , mkHs
- , unHs
- , toHs
  , dummyTypes
- , runInSession
- , runInSessionWith
- , getGhcPkgs
- , haveProg
- , haveNS
  ) where
 
 import Control.Exception (SomeException, try)
@@ -21,38 +12,13 @@ import Data.Char
 import Data.Data
 import Data.Generics.Uniplate.Data
 import Data.Maybe
-import DynFlags
-import ErrUtils
-import qualified GHC
-import GHC.Paths
-import GhcMonad
 import Module
 import Name
-import Outputable
 import Packages
 import System.Directory
 import System.Exit
 import System.Process
 import TypeRep
-
--- Haskell files, with smart constructors
--- | Haskell file paths
-newtype HsFile = Hs FilePath  deriving (Show, Eq, Ord)
-
--- | Turn Strings into HsFiles if they look like Haskell files
-mkHs :: FilePath -> Maybe HsFile
-mkHs f = let ext s = map toLower (take (length s) (reverse f)) == reverse s
-         in  if ext ".hs" || ext ".lhs"
-                then Just (Hs f)
-                else Nothing
-
--- | Turn a list of FilePaths into a list of HsFiles, discarding invalid ones
-toHs :: [FilePath] -> [HsFile]
-toHs = mapMaybe mkHs
-
--- | Get the path of a Haskell file
-unHs :: HsFile -> FilePath
-unHs (Hs f) = f
 
 -- | Arbitrary rose trees
 data Sexpr a = Leaf a | Node [Sexpr a] deriving (Eq, Typeable, Data)
@@ -74,68 +40,6 @@ instance Functor Sexpr where
 instance Show a => Show (Sexpr a) where
   show (Leaf x)  = show x
   show (Node xs) = "(" ++ unwords (map show xs) ++ ")"
-
--- Wrap up the dynamically-dangerous GHC API
-
--- | Use this to start 'Ghc' computations. It is less flexible than 'runGhc',
--- but avoids session-related runtime errors.
-runInSession :: Ghc a -> IO a
-runInSession = runInSessionWith []
-
-runInSessionWith pkgs = GHC.runGhc (Just libdir) . inDefaultEnv pkgs
-
-dieErr flg lvl loc fmt msg = error (show (runSDoc (mkLocMessage lvl loc msg)
-                                                  (initSDocContext flg fmt)))
-
-pkgDbs = do
-  db <- getGhcPkgs
-  return (PkgConfFile db)
-
-getGhcPkgs :: IO FilePath
-getGhcPkgs = do
-  ns <- haveNS
-  if ns then do output <- readProcess "nix-shell" ["--run", "ghc-pkg list"] ""
-                return (takeWhile (/= ':') (head (lines output)))
-        else return ""
-
-haveProg cmd args = do
-  found <- try (readProcessWithExitCode
-                  cmd
-                  args
-                  "") :: IO (Either SomeException (ExitCode, String, String))
-  case found of
-       Left  _ -> print ("Cannot run '" ++ cmd ++ "'") >> return False
-       Right _ -> return True
-
-haveNS = haveProg "nix-shell" ["--help"]
-
-getDirs :: IO [FilePath]
-getDirs = do
-  cwd <- getCurrentDirectory
-  out <- readProcess "find" [cwd, "-type", "d"] ""
-  return (lines out)
-
-inDefaultEnv pkgs x = do oldFlags <- getSessionDynFlags
-                         pkgDb <- liftIO pkgDbs
-                         dirs  <- liftIO getDirs
-                         let newFlags = oldFlags {
-                             log_action   = dieErr
-                           , ghcLink      = LinkInMemory
-                           , hscTarget    = HscInterpreted
-                           -- Include all ghc package's modules, don't rename any
-                           , packageFlags = map exposePkg ("ghc":[]{-pkgs-})
-                           -- Include this package's dependencies
-                           , extraPkgConfs = (pkgDb:)
-                           -- Include all directories, just in case (HACK)
-                           --, includePaths = dirs  -- C header includes?
-                           , importPaths  = dirs
-                           --, libraryPaths = dirs
-                         }
-                         GHC.setSessionDynFlags newFlags
-                         liftIO $ initPackages newFlags
-                         x
-
-exposePkg p = ExposePackage (PackageArg p) (ModRenaming True [])
 
 -- Helpful instances
 instance Show Name where
--- tests/HS2AST/Tests/Generators.hs
+++ tests/HS2AST/Tests/Generators.hs
@@ -1,27 +1,17 @@
 {-# LANGUAGE FlexibleInstances #-}
 
 module HS2AST.Tests.Generators (
-   arbBad
- , unique
- , gen
+   unique
  ) where
 
 import HS2AST.Types
 import Control.Applicative
-import Data.Maybe
 import Data.Data
 import qualified Data.Set as Set
-import GHC
 import System.Directory
-import System.IO.Unsafe
 import Test.QuickCheck
-import Test.QuickCheck.Gen
-import Test.QuickCheck.Monadic
 
 -- Arbitrary instances
-
-instance Arbitrary HsFile where
-  arbitrary = oneof (map return goodFiles)
 
 instance (Data a, Arbitrary a) => Arbitrary (Sexpr a) where
   arbitrary = let f 0 = mkLeaf <$> arbitrary
@@ -35,37 +25,6 @@ unique :: (Ord a) => [a] -> [a]
 unique = Set.toList . Set.fromList
 
 -- Helper functions
-
--- Test data from disk. unsafePerformIO is justified since:
--- a) Test files should remain constant during a run
--- b) Files can't be relied on anyway, since they're only accessible via Gen
-testFiles :: String -> [HsFile]
-testFiles d = let paths = unsafePerformIO (getDirectoryContents d)
-               in mapMaybe (mkHs . (d ++)) paths
-
--- | Haskell files
-goodFiles, badFiles :: [HsFile]
-[goodFiles, badFiles] = map testFiles ["tests/data/good/", "tests/data/bad/"]
-
-{-|
-  Generate paths to invalid Haskell files, for example to test how your
-  parser handles invalid input. Acts like 'arbitrary', but avoids a new type.
--}
-arbBad :: Gen HsFile
-arbBad = oneof (map return badFiles)
-
--- | Runs Ghc monad in IO monad in QuickCheck monad...
-go = run . runInSession
-
--- | Makes debugging easier
-gen n = let rounds = ceiling (fromIntegral n / 11.0)
-            chunks = vectorOf rounds arbitrary
-        in do samples <- sample' chunks
-              return (take n (concat samples))
-
--- Don't rely on this to be stable across runs
-{-# NOINLINE unsafeDynFlags #-}
-unsafeDynFlags = unsafePerformIO (runInSession getSessionDynFlags)
 
 -- | Use a sized generator to generate a list of values whose combined size
 -- matches the given number.
