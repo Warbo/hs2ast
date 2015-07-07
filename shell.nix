32180536562173d1d242996e15d1b2de0453a6a3
Chris Warburton <chriswarbo@gmail.com>
2015-07-07 15:19:47 +0100

    Added attolisp

--- shell.nix
+++ shell.nix
@@ -4,22 +4,22 @@ let
 
   inherit (nixpkgs) pkgs;
 
-  f = { mkDerivation, aeson, ArbitraryHaskell, base, bytestring
-      , containers, directory, ghc, MissingH, process, QuickCheck, stdenv
-      , stringable, syb, tasty, tasty-quickcheck, temporary, transformers
-      , uniplate
+  f = { mkDerivation, aeson, ArbitraryHaskell, atto-lisp
+      , attoparsec, base, bytestring, containers, directory, ghc
+      , MissingH, process, QuickCheck, stdenv, stringable, syb, tasty
+      , tasty-quickcheck, temporary, transformers, uniplate
       }:
       mkDerivation {
         pname = "HS2AST";
         version = "0.1.0.0";
         src = ./.;
         buildDepends = [
-          aeson base bytestring containers directory ghc MissingH process
-          stringable syb temporary transformers uniplate
+          aeson atto-lisp attoparsec base bytestring containers directory ghc
+          MissingH process stringable syb temporary transformers uniplate
         ];
         testDepends = [
-          aeson ArbitraryHaskell base containers directory process QuickCheck
-          tasty tasty-quickcheck
+          aeson ArbitraryHaskell atto-lisp attoparsec base containers
+          directory process QuickCheck tasty tasty-quickcheck
         ];
         homepage = "http://chriswarbo.net/essays/repos/hs2ast.html";
         description = "Dump syntax trees of Haskell code";
