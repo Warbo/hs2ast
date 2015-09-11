8e73a8ba490e88af51b32c0e37712e32f9371b31
Chris Warburton <chriswarbo@gmail.com>
2015-09-11 14:58:20 +0100

    Dependencies for Generators

--- shell.nix
+++ shell.nix
@@ -13,11 +13,12 @@ let
         version = "0.1.0.0";
         src = ./.;
         libraryHaskellDepends = [
-          aeson atto-lisp attoparsec base bytestring ghc stringable syb
+          aeson atto-lisp attoparsec base bytestring derive ghc QuickCheck
+          stringable syb
         ];
         testHaskellDepends = [
-          aeson atto-lisp attoparsec base derive ghc QuickCheck stringable
-          tasty tasty-quickcheck
+          aeson atto-lisp attoparsec base ghc QuickCheck stringable tasty
+          tasty-quickcheck
         ];
         homepage = "http://chriswarbo.net/essays/repos/hs2ast.html";
         description = "Dump syntax trees of Haskell code";
